8ca4b40a1d56f86b9cb3e82396bcd88b7005031b
Vitor Alves <78934171+vitoralves22@users.noreply.github.com>
2023-06-10 10:15:36 -0300

    Update calculo-bonus.sol

--- calculo-bonus.sol
+++ calculo-bonus.sol
@@ -2,23 +2,20 @@
 pragma solidity 0.8.20;
 
 contract CalculoBonusVendedor {
-    // Endereço 0xDeCb1d2F2f98E64706609f920e4380aD9D614bb5
+    // Endereço 0xBde6f445B0eAd01a1B6766DF5905B9E7881dA31c
     // Variáveis públicas
     string public nomeVendedor;
     uint256 public fatorBonus;
-    uint256 public valorVenda;
     
     // Função para definir as variáveis
-    function setDados(string memory _nomeVendedor, uint256 _valorVenda, uint256 _fatorBonus) public {
+    function setDados(string memory _nomeVendedor, uint256 _fatorBonus) public {
         nomeVendedor = _nomeVendedor;
-        valorVenda = _valorVenda;
         fatorBonus = _fatorBonus;
     }
     
     // Função que calcula o bônus do vendedor
-    function calcularBonus() public view returns (uint256) {
+    function calcularBonus(uint256 valorVenda) public view returns (uint256) {
         uint256 bonus = valorVenda * fatorBonus / 100;
         return bonus;
     }
-
 }
