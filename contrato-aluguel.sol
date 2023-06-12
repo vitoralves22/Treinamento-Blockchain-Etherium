0c6380ae2d25ace6d0003acae7db89180e89e031
Vitor Alves <78934171+vitoralves22@users.noreply.github.com>
2023-06-12 17:34:59 -0300

    Update contrato-aluguel.sol

--- contrato-aluguel.sol
+++ contrato-aluguel.sol
@@ -6,23 +6,22 @@ pragma solidity 0.8.20;
 contract ContratoAluguel {
     struct Pessoa {
         string nome;
+        address endereco;
     }
 
     Pessoa public locador;
     Pessoa public locatario;
     uint256[36] public valoresAluguel;
     mapping (uint256 => bool) public pagamentos;
-    address public proprietario;
 
-    modifier onlyProprietario() {
-        require(msg.sender == proprietario, "Somente o proprietario pode chamar essa funcao.");
+    modifier onlyLocador() {
+        require(msg.sender == locador.endereco, "Somente o locador pode chamar essa funcao.");
         _;
     }
 
-    constructor(string memory _nomeLocador, string memory _nomeLocatario, uint256 valorInicialAluguel) {
-        locador = Pessoa(_nomeLocador);
-        locatario = Pessoa(_nomeLocatario);
-        proprietario = msg.sender;
+    constructor(string memory _nomeLocador, string memory _nomeLocatario, address _enderecoLocador, uint256 valorInicialAluguel) {
+        locador = Pessoa(_nomeLocador, _enderecoLocador);
+        locatario = Pessoa(_nomeLocatario, address(0));
 
         for (uint i = 0; i < 36; i++) {
             valoresAluguel[i] = valorInicialAluguel;
@@ -39,7 +38,7 @@ contract ContratoAluguel {
         return (locador.nome, locatario.nome);
     }
 
-    function alteraNome(uint8 tipoPessoa, string memory novoNome) public onlyProprietario {
+    function alteraNome(uint8 tipoPessoa, string memory novoNome) public onlyLocador {
         require(tipoPessoa == 1 || tipoPessoa == 2, "Tipo de pessoa invalido.");
 
         if (tipoPessoa == 1) {
@@ -49,7 +48,7 @@ contract ContratoAluguel {
         }
     }
 
-    function reajustaAluguel(uint256 mesInicio, uint256 valorReajuste) public onlyProprietario {
+    function reajustaAluguel(uint256 mesInicio, uint256 valorReajuste) public onlyLocador {
         require(mesInicio > 0 && mesInicio <= 36, "Mes de inicio invalido.");
 
         for (uint i = mesInicio - 1; i < 36; i++) {
