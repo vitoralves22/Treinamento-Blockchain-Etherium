9083ae2aca0221a6e68368e003e651b34af70b2a
Vitor Alves <78934171+vitoralves22@users.noreply.github.com>
2023-06-12 17:24:21 -0300

    Update contrato-aluguel.sol

--- contrato-aluguel.sol
+++ contrato-aluguel.sol
@@ -1,57 +1,52 @@
 // SPDX-License-Identifier: MIT
-// Endereço: 0x0c3c1a615A4910fe9ce815085f2fE24Cb48a8542
+// Endereço: 0xfa4f5D455c21EB2BF1232A030d74579805918eb0
 
 pragma solidity 0.8.20;
 
 contract ContratoAluguel {
-    struct Pessoa {
-        string nome;
-    }
-
-    Pessoa public locador;
-    Pessoa public locatario;
+    string public locador;
+    string public locatario;
     uint256[36] public valoresAluguel;
-    address public proprietario;
+    mapping (uint256 => bool) public pagamentos;
 
-    modifier onlyProprietario() {
-        require(msg.sender == proprietario, "Somente o proprietario pode chamar essa funcao.");
-        _;
-    }
-
-    constructor(string memory _nomeLocador, string memory _nomeLocatario, uint256 valorInicialAluguel) {
-        locador = Pessoa(_nomeLocador);
-        locatario = Pessoa(_nomeLocatario);
-        proprietario = msg.sender;
+    constructor(string memory _locador, string memory _locatario, uint256 valorInicialAluguel) {
+        locador = _locador;
+        locatario = _locatario;
 
         for (uint i = 0; i < 36; i++) {
             valoresAluguel[i] = valorInicialAluguel;
+            pagamentos[i] = false;
         }
     }
 
     function valorAluguel(uint256 mes) public view returns (uint256) {
-        require(mes > 0 && mes <= 36, "Mes invalido.");
         return valoresAluguel[mes - 1];
     }
 
     function getNomes() public view returns (string memory, string memory) {
-        return (locador.nome, locatario.nome);
+        return (locador, locatario);
     }
 
-    function alteraNome(uint8 tipoPessoa, string memory novoNome) public onlyProprietario {
-        require(tipoPessoa == 1 || tipoPessoa == 2, "Tipo de pessoa invalido.");
-
+    function alteraNome(uint8 tipoPessoa, string memory novoNome) public {
         if (tipoPessoa == 1) {
-            locador.nome = novoNome;
+            locador = novoNome;
         } else if (tipoPessoa == 2) {
-            locatario.nome = novoNome;
+            locatario = novoNome;
         }
     }
 
-    function reajustaAluguel(uint256 mesInicio, uint256 valorReajuste) public onlyProprietario {
-        require(mesInicio > 0 && mesInicio <= 36, "Mes de inicio invalido.");
-
+    function reajustaAluguel(uint256 mesInicio, uint256 valorReajuste) public {
         for (uint i = mesInicio - 1; i < 36; i++) {
             valoresAluguel[i] += valorReajuste;
         }
+    }
+
+    function pagarAluguel(uint256 mes) public {
+        require(!pagamentos[mes - 1], "Aluguel deste mês já foi pago.");
+        pagamentos[mes - 1] = true;
+    }
+
+    function verificarPagamento(uint256 mes) public view returns (bool) {
+        return pagamentos[mes - 1];
     }
 }
