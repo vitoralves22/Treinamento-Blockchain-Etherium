1e4736f4647d7e73bdd2aa7a9e89fb69e743835c
Vitor Alves <78934171+vitoralves22@users.noreply.github.com>
2023-06-16 17:23:55 -0300

    Update token-ERC20.sol

--- token-ERC20.sol
+++ token-ERC20.sol
@@ -1,13 +1,13 @@
 // SPDX-License-Identifier: MIT
-// Endereço: 0x04524B8FfC78d5c89Fa8D182d0C49dC0edB8BAc6
+// Endereço: 0xd9145CCE52D386f254917e481eB44e9943F39138
 
 pragma solidity 0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-contract MyToken is ERC20, ERC20Burnable {
-    constructor(uint256 initialSupply) ERC20("MyToken", "MTK") {
+contract VitorToken is ERC20, ERC20Burnable {
+    constructor(uint256 initialSupply) ERC20("VitorToken", "MTK") {
         _mint(msg.sender, initialSupply);
     }
 }
